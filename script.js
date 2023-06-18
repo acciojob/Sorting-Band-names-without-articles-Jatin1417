63626152286cd99a4c505dc9bc0f440f3c493bd5
Jatin1417 <116420165+Jatin1417@users.noreply.github.com>
2023-06-18 19:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+let bands = ["red","blue","pink","yellow","green"];
+bands.sort();
+let mainUl = document.getElementById("band");
+
+bands.foreach(element => {
+	let child = document.createElement("li");
+	child.innerText = element;
+	mainUl.appendchild(li);
+})
+
+
+
 
